191a0410022a066a1cb1d28dcaca2faa591963cc
SinghALGO <karanssandhu92@gmail.com>
2024-03-19 12:54:18 -0700

    Add: firebase auth for user signIn

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import Header from './Header';
 import {checkValidData} from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword , signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../utils/firebase";
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
@@ -30,6 +30,16 @@ const Login = () => {
   });
     }
     else{
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+    
+    const user = userCredential.user;
+    
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 
     }
   }
