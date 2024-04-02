aa86d76db060bd30e48dd48bf2ef5652cb668a0c
SinghALGO <karanssandhu92@gmail.com>
2024-04-02 00:23:41 -0700

    Add: onMouseMove event added to have scroll functionality for movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useRef } from 'react';
 import MovieCard from './MovieCard';
 const MovieList = ({title, movies}) => {
+   const scrollContainerRef = useRef(null);
+   const handleMouseMove = (event) => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      const { clientX, clientY } = event;
+      const rect = container.getBoundingClientRect();
+      const { left, top, width, height } = rect;
+      const xRatio = (clientX - left) / width;
+      const scrollSpeed = 30;
+
+      if (xRatio < 0.05) {
+        container.scrollLeft -= scrollSpeed;
+      } else if (xRatio > 0.95) {
+        container.scrollLeft += scrollSpeed;
+      }
+    }
+  };
   return (
     <div className="px-9  ">
        <h1 className="text-3xl py-4 text-white">{title}</h1>
-     <div className="flex overflow-x-scroll">
+     <div className="flex overflow-hidden" ref={scrollContainerRef}  onMouseMove={handleMouseMove}
+        style={{ cursor: 'grab' }}>
      
       <div className="flex">
         {movies?.map(movie => <MovieCard key={movie.id} posterPath={movie.poster_path}/>)}
