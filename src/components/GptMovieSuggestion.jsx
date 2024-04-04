821cd9bb358a51f8dcea0fd006c6f805e9e39586
SinghALGO <karanssandhu92@gmail.com>
2024-04-03 17:05:20 -0700

    Add: MovieList reused in GptMovieSuggestion

--- src/components/GptMovieSuggestion.jsx
+++ src/components/GptMovieSuggestion.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import MovieList from './MovieList';
 const GptMovieSuggestion = () => {
   const gpt = useSelector(store => store.gpt);
   const {movieResults, movieNames} = gpt;
   if(!movieNames) return;
   return (
-    <div className="">
-     <div className=" block p-4 m-4 bg-black text-white flex ">
-      {movieNames.map((movieName, index) => (
-        <p key={index}>{movieName}</p>
-      ))}
-    </div>
+     <div className=" block p-4 m-4 bg-black text-white  ">
+     <div>
+      {movieNames.map((movie,index) => <MovieList key={index} title={movie} movies={movieResults[index]}/>)}
+    
+      </div>
     </div>
   )
 }
