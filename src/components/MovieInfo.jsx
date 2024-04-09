323afe4f330c904541a326758a1fcf9db2b29058
SinghALGO <karanssandhu92@gmail.com>
2024-04-08 22:21:36 -0700

    Add: MovieInfoComponent added to show movie details

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {useSelector} from "react-redux";
 import { useParams } from "react-router-dom";
 import useMovieInfo from "../hooks/useMovieInfo";
+import MovieInfoContainer from "./MovieInfoContainer";
 import Header from "./Header";
 import { IMAGE_CDN } from "../utils/constants";
 import Loader from "./Loader";
@@ -17,9 +18,9 @@ const MovieInfo = () => {
     <div>
       <Header />
       <div className="w-full min-h-[110vh] md:min-h-screen top-0 absolute -z-10 overflow-hidden bg-black">
-        <img className="h-[110vh] md:h-auto object-cover mx-auto brightness-[.3]" src={IMAGE_CDN + info.backdrop_path} alt="moviebg" />
+        <img className="w-full h-[110vh] md:h-auto object-cover mx-auto brightness-[.3]" src={IMAGE_CDN + info.backdrop_path} alt="moviebg" />
       </div>
-      
+       <MovieInfoContainer info={info}/>
     </div>
   );
 };
