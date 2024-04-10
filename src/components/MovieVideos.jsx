46fcd90126a84d0416132d90e125230cdec5ddda
SinghALGO <karanssandhu92@gmail.com>
2024-04-10 12:22:53 -0700

    Add: videos scroll added

--- src/components/MovieVideos.jsx
+++ src/components/MovieVideos.jsx
@@ -1,54 +1,66 @@
-import {useRef} from "react";
+import { useRef } from "react";
 import useVideo from "../hooks/useVideo";
 import { useSelector } from "react-redux";
 
 const MovieVideos = ({ id }) => {
   useVideo(id);
   const movieVideos = useSelector((store) => store.movie.movieVideos);
-   const scrollContainerRef = useRef(null);
-   const handleMouseMove = (event) => {
+  const scrollContainerRef = useRef(null);
+
+  const handleScroll = (direction) => {
     const container = scrollContainerRef.current;
     if (container) {
-      const { clientX, clientY } = event;
-      const rect = container.getBoundingClientRect();
-      const { left, top, width, height } = rect;
-      const xRatio = (clientX - left) / width;
-      const scrollSpeed = 50;
-
-      if (xRatio < 0.05) {
+      const scrollSpeed = 180;
+      if (direction === "left") {
         container.scrollLeft -= scrollSpeed;
-      } else if (xRatio > 0.95) {
+      } else if (direction === "right") {
         container.scrollLeft += scrollSpeed;
       }
     }
   };
+
   if (!movieVideos || movieVideos.length === 0) return null;
 
   return (
-    
     <div className="bg-black py-4 md:px-14 px-9">
       <div className="md:mb-5 mb-3">
         <span className="text-white font-bold md:text-3xl text-xl">Videos</span>
       </div>
-      <div className="my-5">
-        <div className="flex gap-10 overflow-hidden" ref={scrollContainerRef}  onMouseMove={handleMouseMove}
-        style={{ cursor: 'grab' }}>
+      <div className="my-5 relative">
+        <div
+          className="flex gap-10 overflow-hidden"
+          ref={scrollContainerRef}
+          style={{ cursor: "grab" }}
+        >
           {movieVideos.map((movieVideo) => (
             <div key={movieVideo?.key} className="w-full md:w-auto">
               <iframe
                 className="aspect-video md:w-[560px] md:h-[315px]"
-
-                src={
-                  "https://www.youtube-nocookie.com/embed/" + movieVideo?.key
-                }
+                src={"https://www.youtube-nocookie.com/embed/" + movieVideo?.key}
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
-                allowfullscreen
+                allowFullScreen
                 loading="lazy"
               ></iframe>
             </div>
           ))}
+        </div>
+        <div className="absolute top-0 left-0 flex items-center h-full">
+          <button
+            className="w-12 h-12 rounded-full bg-red-700 text-white flex items-center justify-center absolute left-0 ml-2 pb-1 font-extrabold"
+            onClick={() => handleScroll("left")}
+          >
+            {"<"}
+          </button>
+        </div>
+        <div className="absolute top-0 right-0 flex items-center h-full">
+          <button
+            className="w-12 h-12 rounded-full bg-red-700 text-white flex items-center justify-center absolute right-0 mr-2 pb-1 font-extrabold"
+            onClick={() => handleScroll("right")}
+          >
+            {">"}
+          </button>
         </div>
       </div>
     </div>
